e855e982d01ac0060b5db680e64534d8933fca1a
Gard Kristian Hauge <gard.kristian.hauge@dnb.no>
2024-04-05 09:15:40 +0200

    Add more methods and tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -9,8 +9,41 @@ public class CalculatorTest {
     @Test
     public void testAdd() {
         int expected = 3;
-
         int actual = calculator.add(1, 2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testDivide() {
+        int expected = 5;
+        int actual = calculator.divide(10, 2);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void testDivideDecimal() {
+        int expected = 3;
+        int actual = calculator.divide(10, 3);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void testMultiply() {
+        int expected = 20;
+        int actual = calculator.multiply(10, 2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSubtract() {
+        int expected = 8;
+        int actual = calculator.subtract(10, 2);
 
         assertEquals(expected, actual);
     }
