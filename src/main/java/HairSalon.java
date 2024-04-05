134b09f68510c189ab006bc2b931546a62261d88
Gard Kristian Hauge <gard.kristian.hauge@dnb.no>
2024-04-05 11:50:09 +0200

    Assumption test

--- src/main/java/HairSalon.java
+++ src/main/java/HairSalon.java
@@ -2,25 +2,38 @@ import java.util.Arrays;
 
 public class HairSalon {
 
-    private String[] openingDays = {"Monday", "Tuesday"};
+    private String[] openingDays = { "Monday", "Tuesday" };
+    private int haircutPrice = 30;
+    private Calculator calculator = new Calculator();
 
     // This method should return the openingDays array.
     public String[] getOpeningDays() {
         return openingDays;
     }
 
-    // This method should return true if the weekday passed in is present in the openingDays array.
-    // If not it should return false. If the String passed in does not end in the letters "day",
-    // it should throw an IllegalArgumentException with a message explaining that only weekdays are valid.
+    // This method should return true if the weekday passed in is present in the
+    // openingDays array.
+    // If not it should return false. If the String passed in does not end in the
+    // letters "day",
+    // it should throw an IllegalArgumentException with a message explaining that
+    // only weekdays are valid.
     public boolean isOpen(String weekDay) {
-        if(!weekDay.endsWith("day")) {
+        if (!weekDay.endsWith("day")) {
             throw new IllegalArgumentException("The weekday argument must end in the letters 'day'");
         }
-        if(Arrays.asList(openingDays).contains(weekDay)) {
+        if (Arrays.asList(openingDays).contains(weekDay)) {
             return true;
         } else {
             return false;
         }
     }
-    
+
+    public int getHaircutPrice() {
+        return haircutPrice;
+    }
+
+    public int applyDiscount() {
+        return calculator.divide(haircutPrice, 2) + 5;
+    }
+
 }
